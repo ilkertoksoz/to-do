603edb5f3a538e90c6b9282b8e6e02e6e6ba8ab2
ilkertoksoz <ahmetilkertoksoz@gmail.com>
2024-01-02 23:50:04 +0300

    logging has been centralized via using AOP config logs will be deleted after

--- src/main/java/com/hepsiemlak/todo/ToDoServiceApplication.java
+++ src/main/java/com/hepsiemlak/todo/ToDoServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @ComponentScan(basePackages = "com.hepsiemlak.todo")
+@EnableAspectJAutoProxy
 public class ToDoServiceApplication {
 
 	public static void main(String[] args) {
